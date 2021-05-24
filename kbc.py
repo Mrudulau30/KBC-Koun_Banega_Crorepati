9822e06747bbdf78cc8357dfa63647183fd1c943
Mrudula Umalkar <66896402+Mrudulau30@users.noreply.github.com>
2021-05-24 21:24:44 +0530

    Update kbc.py

--- kbc.py
+++ kbc.py
@@ -25,25 +25,7 @@ def lifeLine(ques):
 
 
 def kbc():
-    ''' Rules to play KBC:
-        * The user will have 15 rounds
-        * In each round, user will get a question
-        * For each question, there are 4 choices out of which ONLY one is correct.
-        * Prompt the user for input of Correct Option number and give feedback about right or wrong.
-        * Each correct answer get the user money corresponding to the question and displays the next question.
-        * If the user is:
-            1. below questions number 5, then the minimum amount rewarded is Rs. 0 (zero)
-            2. As he correctly answers question number 5, the minimum reward becomes Rs. 10,000 (First level)
-            3. As he correctly answers question number 11, the minimum reward becomes Rs. 3,20,000 (Second Level)
-        * If the answer is wrong, then the user will return with the minimum reward.
-        * If the user inputs "lifeline" (case insensitive) as input, then hide two incorrect options and
-            prompt again for the input of answer.
-        * NOTE:
-            50-50 lifeline can be used ONLY ONCE.
-            There is no option of lifeline for the last question( ques no. 15 ), even if the user has not used it before.
-        * If the user inputs "quit" (case insensitive) as input, then user returns with the amount he has won until now,
-            instead of the minimum amount.
-    '''
+    
 
     #  Display a welcome message only once to the user at the start of the game.
     #  For each question, display the prize won until now and available life line.
